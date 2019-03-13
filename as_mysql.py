c4a837c4ae15aed75c37afc48ba5a656bf027482
Zac Sadan <34508832+ZacSadan@users.noreply.github.com>
2019-03-13 15:39:36 +0200

    Update as_mysql.py

--- as_mysql.py
+++ as_mysql.py
@@ -1,13 +1,9 @@
 #!/usr/bin/env python
 '''A module which implements the MySQL server protocol, giving you the ability to create MySQL-like services
 
-It's a ported version of the library: https://github.com/CloudQuote/faux-mysql-server/blob/master/src/index.js
-
 run in the background :
 nohup python36 ./as_mysql.py  >/dev/null 2>&1 &
 
-Windows Debugging:
-/cygdrive/c/Program\ Files\ \(x86\)/Python36-32/python.exe -u as_mysql.py
 
 '''
 
